6e04d8f93f187345a901fa6231a3f08d33ff3cf6
nitikbisht <nitikbisht62@gmail.com>
2022-10-11 22:55:43 +0530

    mens page added

--- scripts/fashion.js
+++ scripts/fashion.js
@@ -1,2 +1,6 @@
 import navbar2 from "../components/navbar2.js";
 document.getElementById("navbar2").innerHTML=navbar2();
+document.getElementById("men").addEventListener("click",()=>{
+    localStorage.setItem("category","men")
+    location.href="./men.html"
+})
---
+++ scripts/men.js
@@ -0,0 +1,2 @@
+import navbar2 from "../components/navbar2.js";
+document.getElementById("navbar2").innerHTML=navbar2();
