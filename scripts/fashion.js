954ec868087a3fbbd1a57cd2ea17f4bfa9d29f7b
nitikbisht <nitikbisht62@gmail.com>
2022-10-12 03:05:21 +0530

    women page added

--- scripts/fashion.js
+++ scripts/fashion.js
@@ -1,6 +1,13 @@
 import navbar2 from "../components/navbar2.js";
 document.getElementById("navbar2").innerHTML=navbar2();
+localStorage.setItem("category","")
 document.getElementById("men").addEventListener("click",()=>{
     localStorage.setItem("category","men")
     location.href="./men.html"
 })
+document.getElementById("women").addEventListener("click",()=>{
+    localStorage.setItem("category","women")
+    location.href="./women.html"
+})
+// import footer from "../footer/footer.js";
+// document.getElementById("footer").innerHTML=footer();
