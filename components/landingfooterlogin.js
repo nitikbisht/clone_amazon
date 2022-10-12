b30c6193353915021d898a90f78da639977d19d4
Luc <68097668+Lucverse@users.noreply.github.com>
2022-10-12 15:10:12 +0530

    Add files via upload

--- components/landingfooterlogin.js
+++ components/landingfooterlogin.js
@@ -4,15 +4,16 @@ function loginGrid2(){
     <div><h2>Your browsing history<a>View or edit your browsing history</a></h2>
     </div>
     <div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div><img src="https://images-eu.ssl-images-amazon.com/images/I/71S8U9VzLTL._AC_UL100_SR100,100_.jpg"></div>
+        <div><img src="https://images-eu.ssl-images-amazon.com/images/I/819Y291erdL._AC_UL100_SR100,100_.jpg"></div>
+        <div><img src="https://images-eu.ssl-images-amazon.com/images/I/61yggJpMNTL._AC_UL100_SR100,100_.jpg"></div>
+        <div><img src="https://images-eu.ssl-images-amazon.com/images/I/71AlNIiiP4L._AC_UL100_SR100,100_.jpg"></div>
+        <div><img src="https://images-eu.ssl-images-amazon.com/images/I/41DKWPH6NoL._AC_UL100_SR100,100_.jpg"></div>
+        <div><img src="https://images-eu.ssl-images-amazon.com/images/I/71VYz+v47bL._AC_UL100_SR100,100_.jpg"></div>
+        <div><img src="https://images-eu.ssl-images-amazon.com/images/I/81vMxjsSmUL._AC_UL100_SR100,100_.jpg"></div>
+        <div><img src="https://images-eu.ssl-images-amazon.com/images/I/41WiV9uk1jL._AC_UL100_SR100,100_.jpg"></div>
+        <div><img src="https://images-eu.ssl-images-amazon.com/images/I/51vjytwuzNL._AC_UL100_SR100,100_.jpg"></div>
+        
     </div>
     `
 }
