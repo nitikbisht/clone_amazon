139bc2403bd86209a6c1b1681278fbf5efa1d42a
Adil-khan-007 <adilk9030@gmail.com>
2022-10-13 14:51:44 +0530

    working on search bar

--- navbar.js
+++ navbar.js
@@ -7,3 +7,23 @@ document.getElementById("crossBtn").addEventListener("click",()=>{
     event.preventDefault();
     document.getElementById("sideBar").style="left : -28%";
 });
+
+document.getElementById("handleSubmit").addEventListener("click",()=>{
+   let input = document.getElementById("search").value;
+   if(input === "mens"){
+    location.href = "";
+   }
+   else if(input === "womens"){
+    location.href = "";
+   }
+   else if(input === "fashion"){
+    location.href = "";
+   }
+   else if(input === "kids"){
+    location.href = "";
+   }
+   else{
+    // write path of home page
+    location.href = "";
+   }
+});
