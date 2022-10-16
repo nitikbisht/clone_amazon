ab0f75f12471fefd589e32c563dbfcb53ebb97c8
Agramom <108925739+Agramom@users.noreply.github.com>
2022-10-16 09:26:03 +0530

    Delete components directory

--- components/fetchdata.js
+++
@@ -1,8 +0,0 @@
-async function fetchdata(){
-    
-    let res=await fetch("http://localhost:3000/products");
-    let data=await res.json();
-    // console.log(data);
-    return data;
-}
-export default fetchdata();
