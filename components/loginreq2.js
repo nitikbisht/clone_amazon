b82779906f908d6a121e747fabd102ffd7593280
Agramom <108925739+Agramom@users.noreply.github.com>
2022-10-17 06:20:11 +0530

    Delete components directory

--- components/loginreq2.js
+++
@@ -1,9 +0,0 @@
-function loginReq2(){
-    return `<div id="landing-footer-login-div">
-    <p>See personalized recommendations</p>
-    <button>Sign in</button>
-    <p>New customer? <a>Start here</a></p>
-</div>
-    `
-}
-export default loginReq2;
