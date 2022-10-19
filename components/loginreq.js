c8a05dcc5d2ef0ef97883a1e173956f5c6ed909f
Agramom <108925739+Agramom@users.noreply.github.com>
2022-10-19 16:25:17 +0530

    Delete components directory

--- components/loginreq.js
+++
@@ -1,9 +0,0 @@
-function loginReq1(){
-    return`
-    <h3>Sign in for you best experience</h2>
-                <div id="landing-login-grid-butt">
-                    <button>Sign in securely</button>
-                </div>
-    `
-}
-export default loginReq1;
